44163ec3c0b7c729e741963915f16cc3e33323a6
Sohamkhadse <sohankhadse532@gmail.com>
2026-04-23 20:32:38 +0530

    first1 code

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,25 @@
-console.log("hey js")
-window.alert("oo come on")
-document.write("good job")  //it use to append(means whatever we write here will appear in the end of the html)
+// let a = 50
+// let b = 5
+
+// let sum = a + b
+// let sub = a - b
+// let div = a / b
+// let mul = a * b
+// let mod = a % b
+
+// console.log(sum)
+// console.log(sub)
+// console.log(div)
+// console.log(mul)
+// console.log(mod)
+
+window.prompt("enter name")
+
+console.log(typeof("prompt"))
+
+let a = "5"
+let b = "5"
+
+let sum = "5" + "5" //contentation
+
+console.log(sum)
---
+++ javascript/start.js
@@ -0,0 +1,3 @@
+console.log("hey js")
+window.alert("oo come on")
+document.write("good job")  //it use to append(means whatever we write here will appear in the end of the html)
