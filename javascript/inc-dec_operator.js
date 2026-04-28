fd0c9d4af4ebf4ca1ded754ef92a8026a3f93f3a
Sohamkhadse <sohankhadse532@gmail.com>
2026-04-28 20:11:50 +0530

    type-casting

---
+++ javascript/inc-dec_operator.js
@@ -0,0 +1,13 @@
+let data = 20
+
+// billu = data++
+// kittu = data++
+let result = 100 + data++
+console.log(data++)
+console.log(result++)
+console.log(result)
+
+let a = 1
+let journey = --a
+console.log(--journey)
+
---
+++ javascript/type-casting.js
@@ -0,0 +1,42 @@
+// number to string
+let a = 1000
+console.log(typeof(a))
+a = String(a)
+console.log(typeof(a))
+console.log(a)
+
+// number to bollean
+let data = 1000
+console.log(typeof(data))
+data = Boolean(data)
+console.log(typeof(data))
+console.log(data)
+
+// string to number(if it has english word it will show Nan means not a number)
+let name = "soham"
+console.log(typeof(name))
+name = Number(name)
+console.log(typeof(name))
+console.log(name)
+
+// string to bollean
+let game = 1000
+console.log(typeof(game))
+game = Boolean(game)
+console.log(typeof(game))
+console.log(game)
+
+// Boolean to string
+let b = true
+console.log(typeof(b))
+b = String(b)
+console.log(typeof(b))
+console.log(b)
+
+// Boolean to number
+let best = false
+console.log(typeof(best))
+best = Number(b)
+console.log(typeof(best))
+console.log(best)
+
