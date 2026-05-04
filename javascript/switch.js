51597b84c09e2868514e76b73cbab2af94d261ed
Sohamkhadse <sohankhadse532@gmail.com>
2026-05-04 22:50:56 +0530

    switch

---
+++ javascript/switch.js
@@ -0,0 +1,42 @@
+//     let first = Number(window.prompt("enter first number :"))
+//     let second = Number(window.prompt("enter second number :"))
+//     let choose = window.prompt("choose 1. add,2. sub,3. mul,4. div,5. mod")
+
+//     switch(choose){
+//     case "1":
+//             console.log(first + second) 
+//             break;
+
+//      case "2":
+//             console.log(first - second) 
+//             break;
+
+//      case "3":
+//             console.log(first * second) 
+//             break;
+     
+//      case "4":
+//             console.log(first / second) 
+//             break;
+
+//      case "5":
+//             console.log(first % second) 
+//             break;
+    
+//     default:
+//         console.log("nothing matched")
+        
+//     }
+
+switch(window.prompt("enter char")){
+       case "a":
+       case "e":
+       case "o":
+       case "i":
+       case "u":
+       console.log("it is vowel")
+       break;
+       default:
+              console.log("consonant")
+}
+    
