6a9eaa0263727851cdc78a18819638cef9f0c5a2
Sohamkhadse <sohankhadse532@gmail.com>
2026-05-10 17:08:32 +0530

    string_manipulation

---
+++ javascript/string.js
@@ -0,0 +1,37 @@
+let string = "soham khadse"
+console.log(string.length)
+
+console.log(string[0])
+console.log(string[1])
+console.log(string[2])
+console.log(string[3])
+console.log(string[4])
+console.log(string[5])
+console.log(string[6])
+console.log(string[7])
+console.log(string[8])
+console.log(string[9])
+console.log(string[10])
+
+console.log(string.charAt(5))
+console.log(string.charAt(7))
+
+console.log(string.indexOf("m"))
+console.log(string.indexOf(" "))
+
+let a = string.substring(0,5)
+console.log(a)
+let b = string.substring(6,18)
+console.log(b)
+console.log(a+" "+b)
+
+let string2 = "jay shri krishna"
+let c = string2.slice(0,3)
+let d = string2.slice(3,8)
+let e = string2.slice(8,40)
+console.log(c+d+e)
+
+let string3= string2.toLowerCase()
+console.log(string3)
+let string4= string2.toUpperCase()
+console.log(string4)
