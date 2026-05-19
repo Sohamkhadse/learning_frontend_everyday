e244f2c18b9341e914bfeae7e6c15e152d3ac80c
Sohamkhadse <sohankhadse532@gmail.com>
2026-05-19 15:20:43 +0530

    array

---
+++ javascript/array.js
@@ -0,0 +1,54 @@
+// let my_array = [1,"bilateral talks sceretary","nagpur","soham khadse"]
+// console.log(my_array.length)
+
+// let num = [1, 4, 8, 12, 16]
+// let square = []
+
+// let i = 0
+
+// while (i < num.length) {
+//     square[i] = num[i] * num[i]
+//     i++
+// }
+
+//     console.log(square)
+
+// let students = [{ name: "student 1", marks: 78 }, { name: "student 2", marks: 81 }, { name: "student 3", marks: 87 }, { name: "student 4", marks: 92 }]
+
+// let obtainedMarks = 0
+
+// for (let i = 0; i < students.length; i++) {
+//     obtainedMarks += students[i].marks
+// }
+
+// console.log(obtainedMarks)
+
+[
+    [
+        [
+            [std1, std2, std12],
+            [std3, std4, std5],
+            [std6, std7, std8],
+            [std9, std10, std11]
+        ]
+        ,
+        [
+            [std1, std2, std12],
+            [std3, std4, std5],
+            [std6, std7, std8],
+            [std9, std10, std11]
+        ]
+    ],
+    [
+        [
+            [std1, std2, std12],
+            [std9, std10, std11]
+        ]
+        ,
+        [
+            [std3, std4, std5],
+            [std6, std7, std8],
+            [std9]
+        ]
+    ]
+]
