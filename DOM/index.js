d6e27e2a9f73889d0e9f4155646690ed80b23063
Sohamkhadse <sohankhadse532@gmail.com>
2026-06-07 20:28:04 +0530

    DOM

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,33 @@
-document.querySelector(".submit").addEventListener("click", function () {
-    window.alert("Remember the password");
-});
+let product = document.getElementById('submit')
+console.log(product)
+
+product.onclick = (event) => {
+    console.log("its submitted");
+}
+
+product.onclick = (event) => {
+    window.alert("its submitted");
+}
+
+console.log(product.onclick)
+
+
+let products = document.querySelector('#submit')
+console.log(products)
+
+let targets = document.querySelectorAll('#submit')
+console.log(targets)
+
+console.log(product.innerHTML)
+
+console.log(product.innerText)
+
+document.getElementById("submit").onclick = function (event) {
+    product.innerHTML = "<b>hello <u>world</u></b>"
+    product.style.background = "red"
+    product.style.color = "white"
+    product.style.border = "none"
+    product.style.outline = "none"
+
+    console.log(event)    
+}
