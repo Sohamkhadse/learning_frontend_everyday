e69f897dede5b6d4c9b42e39e0209d52a9e14168
Sohamkhadse <sohankhadse532@gmail.com>
2026-06-24 20:18:55 +0530

    set_timeout(add)

---
+++ javascript2/set_interval.js
@@ -0,0 +1,20 @@
+// setInterval(()=>{
+//     console.log("hello")
+// },[10000])
+
+// setTimeout(()=>{
+//     console.log("worked")
+// },5000)
+   let container = document.getElementById("container");
+
+        setTimeout(() => {
+            let img = document.createElement("img");
+            img.src = "./house4.jpg";
+            img.width = 500;
+
+            img.addEventListener("click", () => {
+                img.style.display = "none";
+            });
+
+            container.appendChild(img);
+        }, 2000);
