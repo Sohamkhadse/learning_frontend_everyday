53724f573065e35f7c0bac383cf1392a20eecddc
Sohamkhadse <sohankhadse532@gmail.com>
2026-08-17 20:18:22 +0530

    mongodb-delte,update

--- backend/mongodb/controllers/review.controller.js
+++ backend/mongodb/controllers/review.controller.js
@@ -8,6 +8,11 @@ export const reviewInfo = (req, res) => {
                 path: "/write",
                 use: "to write review",
                 method: "post"
+            },
+            "2":{
+                path:"/fetch-all",
+                use: "to real all reviews",
+                method: "get"
             }
         }
     })
@@ -28,5 +33,69 @@ export const write = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ message: "failed to write a review !", error })
+    }
+}
+
+export const fetchReview = async (req, res) => {
+    try {
+
+        let reviews = await reviewModel.find({}, { __v: false })
+
+        if (reviews.length == 0) {
+            throw ({ error: "no reviews to fetch !" })
+        }
+
+        res.status(200).json({ message: "fetched reviews", reviews })
+
+    } catch (error) {
+        res.status(500).json({ message: "failed to fetch a review !", error })
+    }
+}
+
+export const updateReview = async (req, res) => {
+    try {
+
+        let { reviewId } = req.params
+
+        let { name, email, message, displayYou, age } = req.body
+
+        let updateQuery = {}
+
+        if (name !== undefined) updateQuery.name = name
+        if (email !== undefined) updateQuery.email = email
+        if (message !== undefined) updateQuery.message = message
+        if (displayYou !== undefined) updateQuery.displayYou = displayYou
+        if (age !== undefined) updateQuery.age = age
+
+        let result = await reviewModel.updateOne({ "_id": reviewId }, { $set: updateQuery })
+
+        res.status(202).json({ message: "successfully updated the review !" })
+
+    } catch (error) {
+        res.status(500).json({ message: "failed to update a review !", error })
+    }
+}
+
+export const deleteReview = async (req, res) => {
+    try {
+        let { reviewId } = req.params
+
+        let result = await reviewModel.deleteOne({ _id: reviewId })
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({
+                message: "review not found !"
+            })
+        }
+
+        res.status(200).json({
+            message: "review deleted successfully !"
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: "failed to delete review !",
+            error
+        })
     }
 }
--- backend/mongodb/routers/reviews.router.js
+++ backend/mongodb/routers/reviews.router.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { reviewInfo, write } from "../controllers/review.controller.js"
+import { fetchReview, reviewInfo,updateReview, write, deleteReview} from "../controllers/review.controller.js"
 
 const reviewsRouter = express.Router()
 
 reviewsRouter.get("/", reviewInfo)
 reviewsRouter.post("/write", write)
+reviewsRouter.post("/fetch-all", fetchReview)
+reviewsRouter.put("/update/:reviewId", updateReview)
+reviewsRouter.delete("/delete/:reviewId", deleteReview)
 
 export { reviewsRouter }
