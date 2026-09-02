00199f5c2ef2d0ad10a51fd8cf9766582fa0282f
Sohamkhadse <sohankhadse532@gmail.com>
2026-09-02 22:20:21 +0530

    wahhhhhh

--- react/first_react/vite-project/src/App.jsx
+++ react/first_react/vite-project/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-// import "./styles/style.css"
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
-
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+
+import "./styles/style.css"
 
 const App = () => {
 
-  let number = 123
+  // let number = 123
 
   // let style1 = { color: "red", backgroundColor: "black" }
   // let style2 = { color: "blue", backgroundColor: "black" }
@@ -15,11 +15,16 @@ const App = () => {
 
   return (
     <div>
-      {/* <h1 style={style1}>hello world {number + 100}</h1>
-      <h1 style={style2}>hello world</h1>
-      <h1 style={style3}>hello world</h1>
-      <h1 style={style1}>hello world</h1> */}
-      <h1 className='text-danger fs-2 fw-bolder'>hello world</h1>
+
+      {/*
+        <h1 style={style1}>hello world {number + 100}</h1>
+        <h1 style={style2}>hello world</h1>
+        <h1 style={style3}>hello world</h1>
+        <h1 style={style1}>hello world</h1>
+      */}
+
+      <h1 className="first">hello world</h1>
+
     </div>
   )
 }
